4053d34c0049584f703542e151b386142b6465a9
PhucNH <phucnh0302@gmail.com>
2024-03-14 00:12:18 +0700

    create input for player

---
+++ Assets/Scripts/Gameplay/Player/PlayerMovement.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Netcode;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : NetworkBehaviour
+{
+    [SerializeField] private float moveSpeed;
+
+    private Vector2 _moveDirection;
+    
+    //Input system
+    private void Update()
+    {
+       HandleMovement();
+    }
+
+    private void HandleMovement()
+    {
+        var movement = new Vector3(_moveDirection.x, 0f, _moveDirection.y);
+        movement.Normalize();
+        //transform.forward = movement;
+        transform.Translate(movement * (moveSpeed * Time.deltaTime));
+    }
+
+    public void InputPlayer(InputAction.CallbackContext context)
+    {
+        _moveDirection = context.ReadValue<Vector2>();
+    }
+}
